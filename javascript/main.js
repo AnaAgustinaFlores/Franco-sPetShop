c9d94293406cf03601f4dc46c87ddc9b607c8470
Marco-Manco <mmanco@fi.uba.ar>
2022-04-15 17:07:58 -0300

    se presisten cambios den los botones de agregar a carrito despues de recargar pagina en base a si el carrito tiene elementos

--- javascript/main.js
+++ javascript/main.js
@@ -12,7 +12,9 @@ Vue.createApp({
         idDeProductosDeCarrito: [],
         checkBoxesMascotas: [],//creo q no se usa
         valor: 1,
-        count: 0
+        count: 0,
+
+  
 
     }
   },
@@ -22,9 +24,13 @@ Vue.createApp({
           .then(response => response.json())
           .then(data => {
             this.productos = data.response
+            this.productos.forEach(producto => producto.estadoAgregado = false)
+            this.preservarDatosAlRecargar()
             this.juguetes = this.productos.filter(producto => producto.tipo.includes("Juguete"))
             this.medicamentos = this.productos.filter(producto => producto.tipo.includes("Medicamento"))
             this.carrito = JSON.parse(localStorage.getItem("carritoDeCompras"))!=null ? JSON.parse(localStorage.getItem("carritoDeCompras")) : []
+
+            
             
       })
   },
@@ -32,22 +38,46 @@ Vue.createApp({
 
 
   methods: {
+    preservarDatosAlRecargar(){
 
+      if(JSON.parse(localStorage.getItem("carritoDeCompras")) !=null){
+        console.log("entro")
+        JSON.parse(localStorage.getItem("carritoDeCompras")).forEach(productoCarrito =>{
+          
+          let cont = 0
+          while(cont < this.productos.length){
+            if(this.productos[cont]._id == productoCarrito._id){
+              console.log("entre a preservar datos")
+
+              this.productos[cont].stock = this.productos[cont].stock -1
+              console.log(this.productos[cont].stock)
+              this.productos[cont].estadoAgregado = true
+              console.log(this.productos[cont].estadoAgregado)
+              cont = this.productos.length
+              
+            }
+            cont++
+          }
+        })
+      }
+    },
+    
     mostrarCartelito(){
       this.mostrarSaludo = true
     },
     mostrarFormulario(){
       this.mostrarSaludo = false
     },
+
     aniadirACarrito(producto){
-      
+      producto.estadoAgregado = true
       this.idDeProductosDeCarrito = this.carrito.map(producto => producto._id)
       if(!this.idDeProductosDeCarrito.includes(producto._id) && producto.stock > 0){
         producto.stock -= 1
-        //se agrega una propiedad al objeto que ayudara a dibujar el botn - +
         producto.unidadesAComprar = 1
         this.carrito.push(producto)
         localStorage.setItem("carritoDeCompras", JSON.stringify(this.carrito))
+
       }
     },
     finalizarCompra(){
@@ -75,10 +105,12 @@ Vue.createApp({
       let precioTotal = 0
       this.carrito.forEach(producto => precioTotal += this.calcularSubtotal(producto))
       return precioTotal
-    }
+    },
+
 
   },
   computed:{
+    
 
   },   
 }).mount('#app')
